bc4c8a72bc4bfc99e0fc31b1aa634076024c0f12
Andreas Wenzel <andreas.m.wenzel@gmail.com>
2021-08-02 10:44:16 -0700

    feat: reduce template to relevant items for element

--- src/components/organisms/TemplateItems.svelte
+++ src/components/organisms/TemplateItems.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import type { BoundBox } from "src/types";
+  import {
+    applicableOperations,
+    variableForOperation,
+  } from "../../utils/SvgRules";
   import { getContext, onMount } from "svelte";
   import type { Writable } from "svelte/store";
   import { getTemplate } from "../../api";
@@ -9,10 +13,32 @@
   const selectedEl = getContext<Writable<SVGElement>>("selectedEl");
   const indicatorBounds = getContext<Writable<BoundBox>>("indicatorBounds");
 
-  function getRelevantItems(el: SVGElement) {
-    //console.log(el);
-    return template;
+  function getRelevantItems(el: SVGElement, t = {}) {
+    let subTemplate = {};
+    for (const item of Object.keys(t)) {
+      if (t[item].hasOwnProperty("type")) {
+        //item is a leaf element. Check if it can be applied to element and add it to subtemplate
+        if (
+          applicableOperations[el.nodeName]
+            .map((x) => variableForOperation[x])
+            .includes(t[item].type)
+        ) {
+          subTemplate[item] = t[item];
+        }
+      } else {
+        //not a leaf item, add children and recusively search down the tree
+        subTemplate[item] = getRelevantItems(el, t[item]);
+      }
+    }
+    //remove any children without children
+    for (const item of Object.keys(subTemplate)) {
+      if (Object.keys(subTemplate[item]).length == 0) {
+        delete subTemplate[item];
+      }
+    }
+    return subTemplate;
   }
+
   function handleClick() {
     $indicatorBounds = { x: 0, y: 0, w: 0, h: 0 };
     $selectedEl = undefined;
@@ -22,5 +48,5 @@
 <button on:click={handleClick}>Finish</button>
 {#if $selectedEl}
   <h1>{$selectedEl.id}</h1>
+  <TemplateItem item={getRelevantItems($selectedEl, template)} />
 {/if}
-<TemplateItem item={getRelevantItems($selectedEl)} />
