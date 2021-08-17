213171f505ad03141cb9f1b6c473d8c54f6019d1
InsulatorGap <andreas.m.wenzel@gmail.com>
2021-08-17 14:17:18 -0700

    feat: imageType route from db and rename routs

--- src/api/templates.api.ts
+++ src/api/templates.api.ts
@@ -1,7 +1,7 @@
 // TODO: Create an actual api endpoint for this
 export async function getTemplate(name = "Star of the week"): Promise<any> {
     if (typeof window === "undefined") return {}
-    return await fetch(`/api/template/${encodeURI(name)}`).then(r => r.json())
+    return await fetch(`/api/imageTypes/${encodeURI(name)}`).then(r => r.json())
 }
 
 
---
+++ src/routes/api/imageTypes/[name].ts
@@ -0,0 +1,22 @@
+import type { Request } from "@sveltejs/kit";
+import type { Response } from "@sveltejs/kit"
+import { mClient } from "../../../utils/minio";
+
+export const get = async ({ params }: Request) => {
+    
+    //export async function get({ params }: Request) {
+    console.log(params);
+    
+    //TODO: make this use params and catch errors if file doesn't exist
+    const imageType = await new Promise<unknown>((resolve) => {
+        let output = "";
+        mClient.getObject("svg-reports", "sotw/sotw.json", function (err, dataStream) {
+            dataStream.on('data', (chunk) => output += chunk)
+            dataStream.on('end', () => resolve(output));
+        })
+    })
+
+    return {
+        body:imageType
+    }
+}
