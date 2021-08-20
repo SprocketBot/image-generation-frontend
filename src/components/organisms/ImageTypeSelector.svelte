d411a90e079b62e2ade2686833595a6e28f36735
Brian <contact@noimbrian.com>
2021-08-20 18:05:59 -0500

    chore: convert ImageTypeSelector to use await block

--- src/components/organisms/ImageTypeSelector.svelte
+++ src/components/organisms/ImageTypeSelector.svelte
@@ -1,26 +1,25 @@
 <script lang="ts">
-import { onMount } from "svelte";
-
   import { getTemplate, getImageTypes } from "../../api";
   export let imageType;
-  
-  let imageTypes = [];
-  let ready = false;
-  onMount(async () => {
-    imageTypes = await getImageTypes();
-    console.log(imageTypes);
-    ready = true;
-  })
-  async function handleImageType(id="sotw"){
-      imageType = await getTemplate(id);
-    }
+
+  async function handleImageType(id = "sotw") {
+    imageType = await getTemplate(id);
+  }
 </script>
 
-{#if !ready}
+{#await getImageTypes()}
   <h1>fetching image types</h1>
-{:else}
+{:then imageTypes}
   {#each imageTypes as type}
     <h1>{type.title}</h1>
-    <button on:click={()=>{handleImageType(type.id)}}>Select</button>
+    <button
+      on:click={() => {
+        handleImageType(type.id);
+      }}>Select</button
+    >
   {/each}
-{/if}
+{:catch error}
+  <h1>Sorry!</h1>
+  <p>We have encountered an error fetching image types.</p>
+  <pre>{error}</pre>
+{/await}
