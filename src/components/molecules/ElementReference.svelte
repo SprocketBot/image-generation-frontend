05dbf7a05b0859c07ba3cd66b4b8b0da6a5df557
Brian <contact@noimbrian.com>
2021-08-20 18:05:05 -0500

    fix: make element behave better on small screens

--- src/components/molecules/ElementReference.svelte
+++ src/components/molecules/ElementReference.svelte
@@ -29,7 +29,12 @@
     function updateBounds(value: boolean) {
         const rect = ref.getBoundingClientRect();
         if (value) {
-            $indicatorBounds = { x: rect.x, y: rect.y, w: rect.width, h: rect.height, };
+            $indicatorBounds = {
+                x: rect.x,
+                y: rect.y,
+                w: rect.width,
+                h: rect.height,
+            };
         } else {
             // Check that the x matches, if it doesn't something else has been located and we shouldn't interfere
             if ($indicatorBounds.x === rect.x) {
@@ -41,35 +46,32 @@
     $: children = Array.from(ref.children).filter(
         (c) => !hiddenElements.includes(c.nodeName) && c instanceof SVGElement
     ) as SVGElement[];
-
-    
 </script>
 
 <Accordion>
     <div slot="header" class="item" let:shown>
-        <span class="open-indicator"
-            >{shown || !children.length ? "-" : "+"}</span
-        >
-        <span class="name"
-            >{friendlyLookup[ref.nodeName] ?? ref.nodeName}{ref.id &&
-                ` (${ref.id})`}</span
-        >
+        <span class="open-indicator">
+            {shown || !children.length ? "-" : "+"}
+        </span>
+        <span class="name" title={ref.id}>
+            {friendlyLookup[ref.nodeName] ?? ref.nodeName}{ref.id &&
+                ` (${ref.id})`}
+        </span>
         <span class="spacer" />
         <span class="actions">
             {#if selectableElements.includes(ref.nodeName)}
                 <button
-                    on:click|preventDefault|stopPropagation={updateSelection}
-                >
+                    on:click|preventDefault|stopPropagation={updateSelection}>
                     {$selectedEl == ref ? "Unselect" : "Select"}
                 </button>
             {:else}
                 <button
+                    class="outline"
                     on:mousedown|preventDefault|stopPropagation={() =>
                         updateBounds(true)}
                     on:mouseup|preventDefault|stopPropagation={() =>
                         setTimeout(() => updateBounds(false), 250)}
-                    on:click|preventDefault|stopPropagation
-                >
+                    on:click|preventDefault|stopPropagation>
                     Locate
                 </button>
             {/if}
@@ -87,19 +89,22 @@
         @apply pl-6;
     }
     div.item {
-        @apply flex items-center justify-center cursor-pointer;
+        @apply flex items-center justify-center cursor-pointer mb-1 max-w-full;
     }
-    /* span.open-indicator {
-        @apply w-4;
-    } */
     span:not(.spacer) {
         @apply mr-4;
     }
     button {
         @apply bg-primary-500 hover:bg-primary-600 text-sproc_dark_gray-500 p-1 w-20;
     }
+    button.outline {
+        @apply bg-transparent border-primary-500 border-2 text-primary-500;
+    }
+    span.name {
+        @apply overflow-ellipsis whitespace-nowrap min-w-0 overflow-hidden;
+    }
     span.spacer {
-        @apply flex-1 flex pr-5 pl-5;
+        @apply flex-1 flex px-4;
     }
     span.spacer::after {
         content: "";
