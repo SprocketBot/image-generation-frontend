1e3c2bf8a57516f71d459b615ba3d4272b82268f
Andreas Wenzel <andreas.m.wenzel@gmail.com>
2021-10-11 16:51:31 -0700

    feat: gracefully fail when minio not connected

--- src/api/savedInputs.api.ts
+++ src/api/savedInputs.api.ts
@@ -1,6 +1,13 @@
 import { browser } from "$app/env";
 
 export async function getSavedInputs(reportCode: string): Promise<any[]> {
-    if(!browser) return [];
-    return fetch(`/api/outputs/list?reportCode=${reportCode}`).then(r => r.json())
+    if (!browser) return [];
+    let response = await fetch(`/api/outputs/list?reportCode=${reportCode}`);
+    return new Promise((res, rej) => {
+        console.log(response);
+        if (response.status !== 200) {
+            rej();
+        }
+        res(response.json());
+    })
 }
--- src/routes/api/outputs/list.ts
+++ src/routes/api/outputs/list.ts
@@ -7,20 +7,34 @@ export const get = async ({ query }: Request): Promise<EndpointOutput> => {
             status: 400
         }
     }
-    const reports = await new Promise<Array<unknown>>(resolve => {
-        const output = [];
-        mClient.listObjects("svg-reports", `${query.get("reportCode")}/`)
-            .on("data", d => {
-                if (d.name && d.name.endsWith(".svg")) {
-                    output.push(d.name.split("/")[1])
-                }
-            })
-            .on("end", () => resolve(output));
-    })
+    try {
+        const reports = await new Promise<Array<unknown>>((resolve, reject) => {
+            const output = [];
+            mClient.listObjects("svg-reports", `${query.get("reportCode")}/`)
+                .on("data", d => {
+                    if (d.name && d.name.endsWith(".svg")) {
+                        output.push(d.name.split("/")[1])
+                    }
+                })
+                .on("end", () => resolve(output))
+                .on("error", (e) => {
+                    console.log(e);
+                    reject(e)
+                });
+        })
 
-    return {
-        headers: {},
-        status: 200,
-        body: JSON.stringify(reports)
+        return {
+            headers: {},
+            status: 200,
+            body: JSON.stringify(reports)
+        }
     }
+    catch {
+        return {
+            headers: {},
+            status: 500,
+        }
+    }
+
+    
 }
