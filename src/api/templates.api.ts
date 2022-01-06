6eddd63fd6eb8c3c137b6c280c7f52103eac96f5
Andreas Wenzel <andreas.m.wenzel@gmail.com>
2022-01-06 10:11:28 -0800

    feat: working on bar

--- src/api/templates.api.ts
+++ src/api/templates.api.ts
@@ -12,24 +12,52 @@ export async function getImageTypes(): Promise<any[]> {
 
 
 export async function uploadTemplate(svgHTML: string | SVGElement, reportType: string, reportName: string): Promise<any> {
-    if (svgHTML instanceof SVGElement) {
-        svgHTML = svgHTML.outerHTML;
-    }
+    let svgHTMLstr: string;
+    svgHTML instanceof SVGElement ? svgHTMLstr = svgHTML.outerHTML : svgHTMLstr = svgHTML;
+    // if (svgHTML instanceof SVGElement) {
+    //     svgHTMLstr = svgHTML.outerHTML;
+    // }
     const body = {
-        svg: svgHTML,
+        //svg: svgHTML,
         reportType,
         reportName,
     }
-    return await fetch("/api/outputs/upload",
+    console.log(body);
+    const res = await fetch("/api/outputs/upload",
         {
             method: "POST",
             body: JSON.stringify(body),
         })
-        .then(r => r.json())
-}
+         .then(r => r.json())
+    
+    console.log(res);
+    //console.log(await fetch(res));
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+        console.log("UPLOAD SUCCESSFUL");
+        }
+        else {
+        console.log("UPLOAD FAILED");
+        }
+    }
+    };
+
+    xhr.upload.onprogress = function(e) {
+        if (e.lengthComputable) {
+            console.log(e.loaded / svgHTMLstr.length);
+        }
+    };
+
+    xhr.open("PUT", res);
+    xhr.send(svgHTMLstr);
+        return
+    }
 
 export async function downloadImage(filename: string) : Promise<any> {
     if (!browser) return {}
-    let res = await fetch(`/api/outputs/${filename}`).then(r => r.json());
+    const res = await fetch(`/api/outputs/${filename}`).then(r => r.json());
     return res.blob;
 }
--- src/routes/api/outputs/upload.ts
+++ src/routes/api/outputs/upload.ts
@@ -2,10 +2,15 @@ import type { Request, Response } from "@sveltejs/kit"
 import { getClient } from "$utils/server/minio";
 
 export const post = async ({body}: Request): Promise<Response> => {
+    console.log("hit");
+    console.log(body);
     const mClient = getClient();
     const data = JSON.parse(body);
+    console.log(data);
     try {
-        let result = await mClient.putObject("svg-reports", `${data.reportType}/${data.reportName}`, data.svg)
+        //expires in 5 minutes
+        const result = await mClient.presignedPutObject("svg-reports", `${data.reportType}/${data.reportName}`, 60 * 5)
+        console.log(result);
         return {
             headers: {},
             status: 200,
