1e1164f72904b671afb5d013e20410ef579b26b9
Andreas Wenzel <andreas.m.wenzel@gmail.com>
2022-02-21 10:38:33 -0800

    fix: error handling

--- src/api/templates.api.ts
+++ src/api/templates.api.ts
@@ -1,30 +1,31 @@
 import { browser } from "$app/env";
 import { FileManager } from "$src/utils/FileManager";
+import { handleApiResponse } from "./common.api";
 
-export async function getTemplate(id): Promise<any> {
+export async function getTemplate(id: string): Promise<any> {
     if (!browser) return {}
-    return await fetch(`/api/imageTypes/${encodeURI(id)}`).then(r => r.json())
+    return handleApiResponse<any>(await fetch(`/api/imageTypes/${encodeURI(id)}`));
 }
 
-export async function getImageTypes(): Promise<any[]> {
+export async function getImageTypes(): Promise<string[]> {
     if (!browser) return []
-    return await fetch(`/api/imageTypes`).then(r => r.json());
+    return handleApiResponse<string[]>(await fetch(`/api/imageTypes`));
 }
 
-export async function getImagesOfType(type): Promise<any[]> {
+export async function getImagesOfType(type: string): Promise<string[]> {
     if (!browser) return []
-    return await fetch(`api/images/${type}`).then(r=>r.json());
+    return handleApiResponse<string[]>(await fetch(`api/images/${type}`))
 }
 
 
 export async function uploadTemplate(svg: SVGElement, reportType: string, reportName: string): Promise<any> {
-    let svgStr = svg.outerHTML
-    const res = await fetch(`/api/images/${reportType}/${reportName}`, { method: "POST" }).then(r => r.json())
-    return await FileManager.uploadFile(res, new Blob([svgStr], {type:'image/svg+xml'}));
+    const svgStr = svg.outerHTML
+    const res = await handleApiResponse<string>(await fetch(`/api/images/${reportType}/${reportName}`, { method: "POST" }))
+    return await FileManager.uploadFile(res, new Blob([svgStr], { type: 'image/svg+xml' }));
 }
 
-export async function downloadImage(reportType: string, filename:string): Promise<string> {
+export async function downloadImage(reportType: string, filename: string): Promise<string> {
     if (!browser) return ""
-    const res = await fetch(`/api/images/${reportType}/${filename}`).then(r => r.json());
+    const res = await handleApiResponse<any>(await fetch(`/api/images/${reportType}/${filename}`));
     return await (await FileManager.downloadFile(res.getURL, res.size)).text();
 }
