dfd7e979b4de97a4a47faa6932bda534b8cbed89
Andreas Wenzel <andreas.m.wenzel@gmail.com>
2022-02-21 10:39:21 -0800

    chore: error handling

--- src/api/filters.api.ts
+++ src/api/filters.api.ts
@@ -1,10 +1,5 @@
+import { handleApiResponse } from "./common.api";
+
 export const getFilterValues = async (code: string): Promise<any> => {
-    const response = await fetch(`/api/filters/${code}`);
-    return new Promise((res, rej) => {
-        if (response.status !== 200) {
-            rej();
-        }
-        res(response.json());
-    })
-        
+    return handleApiResponse<any>(await fetch(`/api/filters/${code}`));    
 }
