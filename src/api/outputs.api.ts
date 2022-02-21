a449e897d57ea369744ce6bb14794b4205a7789b
Andreas Wenzel <andreas.m.wenzel@gmail.com>
2022-02-21 14:09:24 -0800

    chore: handle response in outputs.api

--- src/api/outputs.api.ts
+++ src/api/outputs.api.ts
@@ -1,9 +1,10 @@
 import { browser } from "$app/env";
 import { FileManager } from "$src/utils/FileManager";
+import { handleApiResponse } from "./common.api";
 
 export async function getOutputs(imageTypeId:string, filename:string): Promise<string[]> {
   if (!browser) return []
-  return await fetch(`api/outputs/${imageTypeId}/${filename}`).then(r=>r.json());
+  return handleApiResponse<string[]>(await fetch(`api/outputs/${imageTypeId}/${filename}`).then(r=>r.json()));
 }
 
 
